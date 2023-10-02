bca24d394a5952ba5ce846d21b4307c32bddf998
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-10-02 21:25:44 +0200

    Update ZaptecSlackNotifier.js

--- ZaptecSlackNotifier.js
+++ ZaptecSlackNotifier.js
@@ -1,18 +1,16 @@
 const axios = require("axios");
 const { WebClient } = require('@slack/web-api');
 
-// You can set this initially for the very first run, but subsequently the token 
-// will be refreshed and updated dynamically.
-const INITIAL_SLACK_TOKEN = 'your_slack_token_here'; // <<< Static first-time token
-
-const SLACK_REFRESH_TOKEN = 'your_slack_refresh_token_here';
-const SLACK_CHANNEL = 'your_slack_channel_id';
-
-const slackClient = new WebClient(INITIAL_SLACK_TOKEN);
-
 const USERNAME = "your_username_here";
 const PASSWORD = "your_password_here";
 let bearerToken;
+
+const INITIAL_SLACK_TOKEN = 'your_slack_token_here'; // Static first-time token
+const SLACK_REFRESH_TOKEN = 'your_slack_refresh_token_here';
+const SLACK_CHANNEL = 'your_slack_channel_id';
+const slackClient = new WebClient(INITIAL_SLACK_TOKEN);
+
+let previousChargerStatuses = {};
 
 async function rotateSlackToken() {
     try {
@@ -24,7 +22,6 @@ async function rotateSlackToken() {
                 refresh_token: SLACK_REFRESH_TOKEN,
             });
             slackClient.token = refreshedTokenData.access_token;
-            console.log("Successfully refreshed Slack refresh token.");
         }
     } catch (error) {
         console.error("Failed to rotate Slack token:", error);
@@ -45,9 +42,9 @@ async function refreshBearerToken() {
         });
 
         bearerToken = response.data.access_token;
-        console.log("Successfully refreshed Zapetc bearer token.");
+        console.log("Successfully refreshed bearer token.");
     } catch (error) {
-        console.error("Failed to refresh Zapetc token:", error);
+        console.error("Failed to refresh token:", error);
     }
 }
 
@@ -66,10 +63,14 @@ async function checkChargerAvailability() {
         console.log(`Found ${chargers.length} chargers.`);
 
         for (let charger of chargers) {
-            if (charger.OperatingMode == 1) {
-                const message = `Charger ${charger.Name} is available!`;
-                console.log(message); // Log to console
-                await notifySlack(message); // Send to Slack
+            const previousStatus = previousChargerStatuses[charger.Id];
+            if (previousStatus !== charger.OperatingMode) {
+                if (charger.OperatingMode == 1) {
+                    const message = `Charger "${charger.Name}" is available!`;
+                    console.log(message);
+                    await notifySlack(message).catch(err => console.error("Failed to send Slack notification:", err));
+                }
+                previousChargerStatuses[charger.Id] = charger.OperatingMode;
             }
         }
     } catch (error) {
@@ -79,18 +80,21 @@ async function checkChargerAvailability() {
 
 async function notifySlack(message) {
     const currentHour = new Date().getHours();
-    if (currentHour >= 6 && currentHour < 17) {
-        try {
-            await slackClient.chat.postMessage({
-                channel: SLACK_CHANNEL,
-                text: message
-            });
-            console.log("Sent Slack notification:", message);
-        } catch (error) {
-            console.error("Failed to send Slack notification:", error);
-        }
-    } else {
-        console.log("Outside notification hours. Skipping Slack notification.");
+
+    // If it's between 17:00 and 06:00, don't send to Slack.
+    if (currentHour >= 17 || currentHour < 6) {
+        console.log("Skipped Slack notification due to current time restrictions.");
+        return;
+    }
+
+    try {
+        await slackClient.chat.postMessage({
+            channel: SLACK_CHANNEL,
+            text: message
+        });
+        console.log("Sent Slack notification:", message);
+    } catch (error) {
+        console.error("Failed to send Slack notification:", error);
     }
 }
 
