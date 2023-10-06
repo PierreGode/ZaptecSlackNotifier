9f5c321b739e8fb4e5b04d23a97b317b5872cb85
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-10-06 17:09:32 +0200

    Update ZaptecSlackNotifier.js

--- ZaptecSlackNotifier.js
+++ ZaptecSlackNotifier.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const { WebClient } = require('@slack/web-api');
 require('dotenv').config();
 
+// Get configuration from environment variables
 const USERNAME = process.env.ZAPTEC_USERNAME;
 const PASSWORD = process.env.ZAPTEC_PASSWORD;
 const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
@@ -11,7 +12,6 @@ const slackClient = new WebClient(SLACK_TOKEN);
 let bearerToken;
 let previousChargerStatuses = {};
 let previousFreeChargerCount = 0;
-let initialRun = true;  // Add this line to establish a flag for initial run
 
 async function refreshBearerToken() {
     console.log("Attempting to refresh Zaptec bearer token...");
@@ -34,9 +34,12 @@ async function refreshBearerToken() {
     }
 }
 
+
+
 async function checkChargerAvailability() {
     console.log("Checking charger availability...");
 
+    const notifications = [];
     const statusIcons = {
         1: ":zaptec-free:",
         3: ":zaptec-charging:",
@@ -45,9 +48,9 @@ async function checkChargerAvailability() {
 
     let availableChargers = [];
     let completedChargers = [];
+    let allChargerStatuses = "";
     let freeChargersCount = 0;
     let chargingStatusChanged = false;
-    let allChargerStatuses = "";
 
     try {
         const response = await axios.get("https://api.zaptec.com/api/chargers", {
@@ -82,22 +85,28 @@ async function checkChargerAvailability() {
             }
         }
 
-        if (chargingStatusChanged && previousFreeChargerCount > freeChargersCount) {
-            const summaryMessage = `${statusIcons[1]} ${freeChargersCount} charger(s) free.`;
-            console.log(summaryMessage);
-            await notifySlack(summaryMessage + "\n" + allChargerStatuses).catch(err => console.error("Failed to send Slack notification:", err));
+        if (availableChargers.length) {
+            const verb = availableChargers.length === 1 ? "is" : "are";
+            const message = `${statusIcons[1]} ${availableChargers.join(", ")} ${verb} available!`;
+            notifications.push(message);
         }
 
-        if (!initialRun) {
-            if (availableChargers.length) {
-                const message = `${statusIcons[1]} ${availableChargers.join(", ")} is/are available!`;
-                console.log(message);
-                await notifySlack(message + "\n" + allChargerStatuses).catch(err => console.error("Failed to send Slack notification:", err));
-            }
+        if (completedChargers.length) {
+            const verb = completedChargers.length === 1 ? "has" : "have";
+            const message = `${statusIcons[5]} ${completedChargers.join(", ")} ${verb} stopped charging.`;
+            notifications.push(message);
+        }
 
-            if (completedChargers.length) {
-                const message = `${statusIcons[5]} ${completedChargers.join(", ")} has/have stopped charging.`;
-                console.log(message);
+        if (chargingStatusChanged && previousFreeChargerCount > freeChargersCount) {
+            const summaryMessage = `${statusIcons[1]} ${freeChargersCount} charger(s) free.`;
+            notifications.push(summaryMessage);
+        }
+
+        previousFreeChargerCount = freeChargersCount;
+
+        if (!initialRun) {
+            for (const message of notifications) {
+                console.log(message + "\n" + allChargerStatuses);
                 await notifySlack(message + "\n" + allChargerStatuses).catch(err => console.error("Failed to send Slack notification:", err));
             }
         } else {
@@ -105,13 +114,27 @@ async function checkChargerAvailability() {
             initialRun = false;  // Reset the flag after the initial run
         }
 
-        previousFreeChargerCount = freeChargersCount;  // Update the previous free charger count
-
     } catch (error) {
         console.error("Failed to fetch charger data:", error);
     }
 }
 
+async function notifySlack(message) {
+    const currentHour = new Date().getHours();
+    if (currentHour >= 16 || currentHour < 6) {
+        console.log("Skipped Slack notification due to current time restrictions.");
+        return;
+    }
+
+    try {
+        await axios.post(SLACK_WEBHOOK_URL, {
+            text: message
+        });
+        console.log("Sent Slack notification:", message);
+    } catch (error) {
+        console.error("Failed to send Slack notification:", error);
+    }
+}
 
 (async () => {
     await refreshBearerToken().catch(err => console.error("Initial token refresh failed:", err));
