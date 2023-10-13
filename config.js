fbb2764d2a7e5d8a96e1926c830189941ca78cf6
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-10-13 13:20:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     startSilentHour: 16,  // Begin silent period at 16:00 (4 PM)
     endSilentHour: 6,    // End silent period at 6:00 (6 AM)
     timeZone: 'Europe/Stockholm', // Set timezone for logging
-    loudStart: 'false', // on service start set if the initial run should be promoting notifications
+    silentStart: false, // on service start set if the initial run should be promoting notifications
     silentDays: ['Saturday', 'Sunday'] // No notifications on Saturday and Sunday
 };
