245e9ad035e8ba10fe9bef6a3c92cac91250daf5
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-10-15 00:48:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
     zaptecUpdateInterval: 3*60*1000, // 3 min, the time in milliseconds between api calls to Zaptec portal
     zaptecTokenRefreshInterval: 24*60*60*1000, // 24h
     silentStart: false, // on service start set if the initial run should be promoting notifications
-    showChargingdata: false,
+    showChargingdata: true,
     iconSet: 2 // 1: unicode circles, 2: unicode icons, 3: slack emojis
 };
