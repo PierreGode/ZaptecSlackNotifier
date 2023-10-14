c7bdf9401744f51c096a2a6a9a6274e443b46d25
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-10-14 12:16:46 +0200

    Added charge history
    Added charge history  push to slack when charge session is complete

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     startSilentHour: 12,  // Begin silent period at 16:00 (4 PM)
     endSilentHour: 13,    // End silent period at 6:00 (6 AM)
     timeZone: 'Europe/Stockholm', // Set timezone for logging
-    silentStart: false, // on service start set if the initial run should be promoting notifications
+    silentStart: true, // on service start set if the initial run should be promoting notifications
     zaptecUpdateInterval: 3*60*1000, // 3 min, the time in milliseconds between api calls to Zaptec portal
     zaptecTokenRefreshInterval: 24*60*60*1000, // 24h
     silentDays: ['Sunday'], // No notifications on Saturday and Sunday
