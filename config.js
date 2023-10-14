2fbbc781eb2f798dcdc42036fc7d913cc3e498d9
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-10-14 11:18:56 +0200

    Added Charge history
    Added Charge history

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
     zaptecUpdateInterval: 3*60*1000, // 3 min, the time in milliseconds between api calls to Zaptec portal
     zaptecTokenRefreshInterval: 24*60*60*1000, // 24h
     silentDays: ['Sunday'], // No notifications on Saturday and Sunday
-    showChargingdata: true,
+    showChargingdata: false,
     iconSet: 2 // 1: unicode circles, 2: unicode icons, 3: slack emojis
 };
